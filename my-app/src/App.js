a58dd2eb3ec09d778924df0baeb500a0c4b985fc
nloncar177 <nloncar177@gmail.com>
2024-04-08 14:51:21 +0200

    završne promjene

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,44 +1,41 @@
 
 import './App.css';
 import { NavButton } from './Botuni';
-import { NewsButton } from './Botuni';
-import { DarkMode } from './Botuni';
-import { NoviNews } from './Botuni';
+import { End } from './Botuni';
 
 
 
+     //<NavButton naziv="Životopis/CV"/>
+     //<NavButton naziv="Illustrator"/>
+     //<NavButton naziv="Photoshop"/>
 
 function Nav() {
   return (
+    <div>
+
     <div className="nav">
 
-     <h1>VIJESTI  </h1>
-     <NavButton naziv="Vijesti"/>
-     <NavButton naziv="Politika"/>
-     <NavButton naziv="Sport"/>
-
-     <DarkMode/>
-
+     <h1 className="navSlov"> Niko Lončar  </h1>
+     
     </div>
-    
+
+
+<div className='textt prim'>
+
+</div>
+
+
+
+<div>
+<End/>
+</div>
+
+
+</div>
   );
 }
 
 
-
-
-function NewsButtons(){
-
-  return (
-    <div className="botuniVijestiON"> 
-     <NewsButton naziv="N1 Vijesti" />
-     <NewsButton naziv="Slobodna Dalmacija" />
-     <NewsButton naziv="24 Sata" />
-     <NoviNews/>
-    </div>
-  );
-
-}
 
 
 
@@ -48,7 +45,6 @@ function App(){
 return(
 <div data-theme>
 <Nav/>
-<NewsButtons/> 
 </div>
 )
 }
--- my-app/src/Botuni.js
+++ my-app/src/Botuni.js
@@ -4,36 +4,6 @@ import React, { useState } from "react";
 //import useLocalStorage from 'use-local-storage'
 
 
-export const DarkMode = () => {
-
-    // darkmode
-    const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-    //const [theme, setTheme] = useLocalStorage('theme', defaultDark ? 'dark' : 'light');
-    const [theme, setTheme] = useState('theme', defaultDark ? 'dark' : 'light');
-  
-    const switchTheme = () =>{ console.log("ssx");
-    const newTheme = theme === "light" ? "dark" : "light";
-    setTheme(newTheme); console.log(newTheme);
-    }
-
-
-    return (
-      <div className="toggle-theme-wrapper">
-        <span>☀️</span>
-        <label className="toggle-theme" htmlFor="checkbox">
-          <input
-            type="checkbox"
-            id="checkbox"
-            onClick={switchTheme}
-          />
-          <div className="slider round"></div>
-        </label>
-        <span>🌒</span>
-      </div>
-    );
-};
-
-
 export function NavButton( { naziv } ){
     return(
     <button className="navBotun">
@@ -45,32 +15,17 @@ export function NavButton( { naziv } ){
 
 
 
-  export function NewsButton( { naziv } ){
-    return(
-    <button className="vstBotun">
-    {naziv}
-  </button>
-  )
-  }
+export function End(){
 
-
-
-  export function NoviNews(){
+  return(
   
-    return (
-      <div className="">
-        <span></span>
-        <label className="toggle-theme" htmlFor="checkbox">
-          <input
-            type="checkbox"
-            id="checkbox"
-          />
-          <div className="slider round"></div>
-        </label>
-        <span></span>
-      </div>
-    );
-  }
+    <div>
+      STAVI INFO
+    </div>
+    
+  )
+
+}
 
 
 
